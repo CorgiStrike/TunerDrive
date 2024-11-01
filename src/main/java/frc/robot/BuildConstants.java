6d1b32e727fd37e1a279b0f684d78de3c1ab2bb6
CorgiStrike <martysanin@gmail.com>
2024-11-01 10:17:18 -0500

    Revert "Update BuildConstants.java"
    This reverts commit ca71275edad63194b122c9b2c46cb298e9d7583a.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "MartinVision";
+  public static final String MAVEN_NAME = "TunerDrive";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 44;
-  public static final String GIT_SHA = "e263ffd79252ae874b083bb047b5041c29a5e628";
-  public static final String GIT_DATE = "2024-10-31 08:00:13 EDT";
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "39d9875ad6e038b9c9d63b0d208859426653986a";
+  public static final String GIT_DATE = "2024-10-30 19:04:43 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-11-01 11:15:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1730474110085L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-10-30 19:11:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1730329890819L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
