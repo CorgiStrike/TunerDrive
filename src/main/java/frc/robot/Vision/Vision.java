da39c7ae05b891d4170f63a98c2700b40f628001
MooreCoder27 <137838319+Jzmoore27@users.noreply.github.com>
2024-11-13 17:36:33 -0500

    Vision init

--- src/main/java/frc/robot/Vision/Vision.java
+++ src/main/java/frc/robot/Vision/Vision.java
@@ -1,23 +1,24 @@
 package frc.robot.Vision;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import frc.robot.Constants;
+import static frc.robot.Constants.Vision.*;
 
-// UNFINISHED - COMMENTING AND FORMATING AFTER FINISHING
 public class Vision {
     private final AprilTagCamera[] pvCams;
     private Field2d feild = new Field2d();
+    private Pose2d prevPose = new Pose2d();
 
     public Vision(List<PVCamera> camSettings ) {
         pvCams = 
@@ -30,23 +31,13 @@ public class Vision {
         
     }
 
-    public Field2d getFeild() {
-        return feild;
+    public Pose2d getRobotPose() {
+        Optional<EstimatedRobotPose> pose = pvCams[0].getEstimatedGlobalPose(prevPose);
+        Pose3d robotPose = pose.get().estimatedPose;
+        prevPose = new Pose2d(robotPose.getX(), robotPose.getY(), new Rotation2d(robotPose.getRotation().getZ()));
+        return prevPose;
     }
 
-    public EstimatedRobotPose getRobotPose() {
-        Double ambiguity = 1.0;
-        EstimatedRobotPose pose = null;
-        for(var cam : pvCams){
-            EstimatedRobotPose tempPose = cam.getCameraPose();
-            if( cam.getPoseAmbiguity() < ambiguity && tempPose != null){
-                pose = tempPose;
-                ambiguity = cam.getPoseAmbiguity();
-            }
-        }
-        return pose;
-    }
-    
     // camera settings - define in Constants
     public record PVCamera(
         String name,
@@ -56,69 +47,19 @@ public class Vision {
 
     // pipeline-specific stuff
     public class AprilTagCamera {
-        private PhotonCamera camera;                // pv pipeline provider
-        private Pose3d camPose;           // poses to return, last pose for fallback
-        private PhotonPoseEstimator poseEstimator;  // pose estimator
-        private Double ambiguityThreshhold;         // 0-1, how ambiguous a tag can be before it is ignored
-        private EstimatedRobotPose lastEstimation;  // last estimated pose from photonvision
-
-        // constructor
+        private PhotonPoseEstimator photonPoseEstimator;
         public AprilTagCamera(PVCamera cam) {
-            // set class vars
-            this.camera = new PhotonCamera(cam.name);
-            this.camera.setDriverMode(false);
-            this.camPose = cam.pose;
-            this.ambiguityThreshhold = cam.ambiguityThreshhold;
-            this.lastEstimation = new EstimatedRobotPose(null, 0, null, null); // Default null pose
-            // create pose estimator
-            this.poseEstimator = new PhotonPoseEstimator(
-                        Constants.Vision.FIELD_LAYOUT,
-                        PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,    // change layout in Constants
-                        camera,
-                        new Transform3d(camPose.getTranslation(),
-                        camPose.getRotation()));
-            poseEstimator.setMultiTagFallbackStrategy(PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY);
+            PhotonCamera camera = new PhotonCamera(cam.name);
+            this.photonPoseEstimator = new PhotonPoseEstimator(
+                FIELD_LAYOUT,
+                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+                camera,
+                new Transform3d(new Pose3d(), cam.pose));
         }
 
-        // get robot pose from ONE camera
-        public EstimatedRobotPose getCameraPose() {
-            PhotonPipelineResult result = camera.getLatestResult();
-            if (result.hasTargets()) {  // make sure tags exist, otherwise error is thrown and both robot AND photonvision crashes
-                for(var target : result.getTargets()){
-                    Double ambiguity = target.getPoseAmbiguity();
-                    if(ambiguity > this.ambiguityThreshhold || ambiguity == -1){
-                        result.targets.remove(target);  // remove target if it is too ambiguous
-                    }
-                }
-                if(!result.hasTargets()){
-                    return lastEstimation;    // return last pose if all targets were too ambiguous
-                }
-                PhotonTrackedTarget tag = result.getBestTarget();
-                // make sure correct field/tags are being used
-                if (Constants.Vision.FIELD_LAYOUT.getTagPose(tag.getFiducialId()).isPresent()) {
-                    var est = poseEstimator.update();
-                    if(!est.isEmpty()) {
-                        lastEstimation = est.get();
-                        var rPose = lastEstimation.estimatedPose;
-                        lastEstimation = new EstimatedRobotPose(new Pose3d(rPose.getX(),rPose.getY(),rPose.getZ(),new Rotation3d(0,rPose.getRotation().getY(),rPose.getRotation().getZ()+this.camPose.getRotation().getZ())), lastEstimation.timestampSeconds, lastEstimation.targetsUsed, lastEstimation.strategy);
-                    }
-                }
-            }
-            return lastEstimation;
-        }
-
-        // get last pose ambiguity
-        public Double getPoseAmbiguity() {
-            Double avgAmbiguity = 0.0;
-            Integer numTargets = 0;
-            if(lastEstimation==null) return 1.0;
-            if (lastEstimation.targetsUsed==null) return 1.0;
-            for(var tag : lastEstimation.targetsUsed) {
-                avgAmbiguity += tag.getPoseAmbiguity();
-                numTargets++;
-            }
-            avgAmbiguity /= numTargets;
-            return avgAmbiguity;
-        }
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+        photonPoseEstimator.setLastPose(prevEstimatedRobotPose);
+        return photonPoseEstimator.update();
+    }
     }
 }
