7bc0f793a41983dd7b57a8240c73e39b649a9f9d
CorgiStrike <martysanin@gmail.com>
2024-11-16 20:16:26 -0500

    make indexer exist in main again (yay!)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,11 @@ import com.ctre.phoenix6.Utils;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.SMF.StateMachine;
 import frc.robot.controllers.RealControllerBindings;
 import frc.robot.generated.TunerConstants;
@@ -37,34 +38,29 @@ public class RobotContainer extends StateMachine<RobotContainer.State>{
   private void configureBindings() {
     drivetrain.configureBindings(controllerBindings::getDriveXValue, controllerBindings::getDriveYValue, controllerBindings::getDriveTurnValue);
 
-    // reset the field-centric heading on left bumper press
+    // reset the field-centric heading on right flight stick, left bumper press
     controllerBindings.resetGyro().onTrue(drivetrain.runOnce(() -> drivetrain.swerveDrive.seedFieldRelative()));
 
     //ground intake on A button
     controllerBindings.manualIntake()
     .onTrue(transitionCommand(State.GROUND_INTAKE, false))
-    .onFalse(
-            new ConditionalCommand(
-                transitionCommand(State.TRAVERSING, false),
-                Commands.none(),
-                () -> getState() == State.GROUND_INTAKE));
+    .onFalse(transitionCommand(State.TRAVERSING));
 
     controllerBindings.autoIntake()
     .onTrue(transitionCommand(State.AUTO_GROUND_INTAKE, false))
-    .onFalse(
-            new ConditionalCommand(
-                transitionCommand(State.TRAVERSING, false),
-                Commands.none(),
-                () -> getState() == State.AUTO_GROUND_INTAKE));
+    .onFalse(transitionCommand(State.TRAVERSING));
 
     //ground eject on B button
     controllerBindings.intakeEject()
     .onTrue(transitionCommand(State.GROUND_EJECT, false))
-    .onFalse(
-      new ConditionalCommand(
-          transitionCommand(State.TRAVERSING, false),
-          Commands.none(),
-          () -> getState() == State.GROUND_EJECT));
+    .onFalse(transitionCommand(State.TRAVERSING));
+    
+    controllerBindings.cleanseIndexer()
+    .onTrue(transitionCommand(State.CLEANSE, false))
+    .onFalse(transitionCommand(State.TRAVERSING));
+
+    controllerBindings.stow()
+    .onTrue(transitionCommand(State.TRAVERSING));
 
     if (Utils.isSimulation()) {
       drivetrain.swerveDrive.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
@@ -90,6 +86,8 @@ public class RobotContainer extends StateMachine<RobotContainer.State>{
   }
 
   private void registerStateTransitions() {
+    //talk to Jonah about changing these to omnis? - maybe disallow the transition from auto ground intake to ground intake - Jonah, if I
+    //forget to talk abt this w/ you, please remind me
     addTransition(State.TRAVERSING, State.AUTO_GROUND_INTAKE);
     addTransition(State.TRAVERSING, State.GROUND_INTAKE);
     addTransition(State.TRAVERSING, State.GROUND_EJECT);
@@ -97,6 +95,9 @@ public class RobotContainer extends StateMachine<RobotContainer.State>{
 
     addOmniTransition(State.SOFT_E_STOP);
     addOmniTransition(State.TRAVERSING);
+    addOmniTransition(State.LOST_NOTE);
+    addOmniTransition(State.STUCK_NOTE);
+    addOmniTransition(State.CLEANSE);
   }
 
   private void registerStateCommands() {
@@ -110,17 +111,17 @@ public class RobotContainer extends StateMachine<RobotContainer.State>{
       new ParallelCommandGroup(
         drivetrain.transitionCommand(CommandSwerveDrivetrain.State.TRAVERSING),
         intake.transitionCommand(Intake.State.INTAKING),
-        indexer.transitionCommand(Indexer.State.INDEXING)
+        indexer.transitionCommand(Indexer.State.AWAITING_NOTE_BACK)
       ),
-      indexer.waitForState(Indexer.State.HAS_NOTE),
+      indexer.waitForState(Indexer.State.INDEXING),
       transitionCommand(State.TRAVERSING)));
     
     registerStateCommand(State.AUTO_GROUND_INTAKE, new SequentialCommandGroup(
       new ParallelCommandGroup(
         drivetrain.transitionCommand(CommandSwerveDrivetrain.State.AUTO_INTAKE),
         intake.transitionCommand(Intake.State.INTAKING),
-        indexer.transitionCommand(Indexer.State.INDEXING)),
-      indexer.waitForState(Indexer.State.HAS_NOTE),
+        indexer.transitionCommand(Indexer.State.AWAITING_NOTE_BACK)),
+      indexer.waitForState(Indexer.State.INDEXING),
       transitionCommand(State.TRAVERSING)));
 
     registerStateCommand(State.GROUND_EJECT, new ParallelCommandGroup(
@@ -129,16 +130,63 @@ public class RobotContainer extends StateMachine<RobotContainer.State>{
         indexer.transitionCommand(Indexer.State.IDLE)
       ));
     
-    registerStateCommand(State.TRAVERSING, new ParallelCommandGroup(
-      drivetrain.transitionCommand(CommandSwerveDrivetrain.State.TRAVERSING),
-      intake.transitionCommand(Intake.State.IDLE),
-      indexer.transitionCommand(Indexer.State.IDLE)
+    registerStateCommand(State.TRAVERSING, 
+    new SequentialCommandGroup(
+      new ParallelCommandGroup(
+        drivetrain.transitionCommand(CommandSwerveDrivetrain.State.TRAVERSING),
+        intake.transitionCommand(Intake.State.IDLE),
+        indexer.transitionCommand(Indexer.State.IDLE)
+      ),
+      new WaitUntilCommand(() -> indexer.getState() == Indexer.State.LOST_NOTE),
+      transitionCommand(State.LOST_NOTE)                                                                                              
     ));
+
+    registerStateCommand(State.CLEANSE, 
+      new SequentialCommandGroup(
+        new ParallelCommandGroup(
+          drivetrain.transitionCommand(CommandSwerveDrivetrain.State.TRAVERSING),
+          intake.transitionCommand(Intake.State.IDLE),
+          indexer.transitionCommand(Indexer.State.PASS_THROUGH)
+        ),
+        new WaitCommand(4),
+        new ConditionalCommand(
+          transitionCommand(State.TRAVERSING), 
+          transitionCommand(State.STUCK_NOTE), 
+          () -> indexer.getState() == Indexer.State.IDLE)
+      )
+    );
+
+    registerStateCommand(State.LOST_NOTE, 
+      new SequentialCommandGroup(
+        new ParallelCommandGroup(
+          //set LEDs to an error color
+          drivetrain.transitionCommand(CommandSwerveDrivetrain.State.TRAVERSING),
+          intake.transitionCommand(Intake.State.IDLE),
+          indexer.transitionCommand(Indexer.State.IDLE)
+        ),
+        new WaitCommand(2.5), //provide the drivers some time to see the error LEDs
+        transitionCommand(State.TRAVERSING)
+      )
+      
+    );
+
+    registerStateCommand(State.STUCK_NOTE, 
+    new SequentialCommandGroup(
+      new ParallelCommandGroup(
+        //set LEDs to an error color
+        drivetrain.transitionCommand(CommandSwerveDrivetrain.State.TRAVERSING),
+        intake.transitionCommand(Intake.State.IDLE),
+        indexer.transitionCommand(Indexer.State.IDLE)
+      ),
+      new WaitUntilCommand(() -> indexer.getState() == Indexer.State.IDLE),
+      transitionCommand(State.TRAVERSING)
+    )
+    );
   }
 
   @Override
   protected void determineSelf() {
-    setState(State.SOFT_E_STOP);
+    setState(State.TRAVERSING);
   }
 
   @Override
@@ -157,6 +205,9 @@ public class RobotContainer extends StateMachine<RobotContainer.State>{
     TRAVERSING,
     AUTO_GROUND_INTAKE,
     GROUND_INTAKE,
-    GROUND_EJECT
+    GROUND_EJECT,
+    STUCK_NOTE,
+    LOST_NOTE,
+    CLEANSE
   }
 }
