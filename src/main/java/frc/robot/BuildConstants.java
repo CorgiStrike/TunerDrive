aa056c037c423a6b57262a49aa408453e76e94c5
Breadspeed1 <53412269+Breadspeed1@users.noreply.github.com>
2024-11-18 20:20:09 -0500

    build constants lol

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,7 +7,7 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TunerDrive";
   public static final String VERSION = "unspecified";
-<<<<<<< Updated upstream
+
   public static final int GIT_REVISION = 69;
   public static final String GIT_SHA = "569480552f2fb48078a30a48a03992f86eac650b";
   public static final String GIT_DATE = "2024-11-18 16:45:45 EST";
@@ -15,15 +15,7 @@ public final class BuildConstants {
   public static final String BUILD_DATE = "2024-11-18 18:08:01 EST";
   public static final long BUILD_UNIX_TIME = 1731971281509L;
   public static final int DIRTY = 1;
-=======
-  public static final int GIT_REVISION = 61;
-  public static final String GIT_SHA = "49ae38453464c76d6c0164cf29e79b015239e7cc";
-  public static final String GIT_DATE = "2024-11-17 19:18:34 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-11-18 20:15:31 EST";
-  public static final long BUILD_UNIX_TIME = 1731978931552L;
-  public static final int DIRTY = 0;
->>>>>>> Stashed changes
+
 
   private BuildConstants(){}
 }
